ad0a65f9242c27b81775d7d75bb3d8dc36fe1e91
Michael Alves <michael.paco.alves@gmail.com>
2018-10-02 23:24:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,87 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import TextField from '@material-ui/core/TextField';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import marked from 'marked';
 
-class App extends Component {
+
+
+marked.setOptions({
+  breaks: true,
+});
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 750,
+
+  },
+  dense: {
+    marginTop: 50,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+class Markdown extends React.Component {
+  state = {
+    preview: ''
+  }
+  getMarkdownText(textData) {
+    const preview = marked(textData);
+    return { __html: preview }
+  }
   render() {
     return (
+      <div dangerouslySetInnerHTML={this.getMarkdownText(this.props.textData)} />
+    )
+  }
+}
+
+class App extends React.Component {
+
+  state = {
+    multiline: '',
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <TextField
+
+          id="editor"
+          label="Code Editor"
+          multiline
+          rows="10"
+          value={this.state.multiline}
+          onChange={this.handleChange('multiline')}
+          className={classes.textField}
+          margin="normal"
+        />
+        <div id='preview'>
+          <Markdown textData={this.state.multiline} />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(App);
